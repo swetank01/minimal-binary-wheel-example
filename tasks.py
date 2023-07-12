dce9fa87ce74ed6f70f6e6d5212b3fb49ba7cd1d
Mike Wilkerson <mike.wilkerson@solas.ai>
2023-07-12 13:08:44 -0400

    Fix typo and add comment explaining global var

--- tasks.py
+++ tasks.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 from invoke import task
 
+# The name of the Python module we are compiling. If you have multiple
+# root-level modules, you will need to perform Nuitka compilation on each
+# module, and package all resulting .so/.pyd files into the wheel
 PACKAGE_NAME = "binary_wheel_example"
 
 
@@ -13,7 +16,7 @@ def compile(c):
 
     Note that we configure Nuitka to avoid creating a .pyi file. This is
     because Nuitka .pyi files are not as fully-featured as what can be created
-    with e.g. `stubgen`, sd we leave this to a separate task.
+    with e.g. `stubgen`, so we leave this to a separate task.
     """
     c.run(
         f"python -m nuitka {PACKAGE_NAME} \
